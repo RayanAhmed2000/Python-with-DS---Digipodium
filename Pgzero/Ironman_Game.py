4969a8413307c94306417886707d670db2f250ef
RayaN <rayanahmed4334@gmail.com>
2022-08-29 13:27:50 +0530

    commit

--- Pgzero/Ironman_Game.py
+++ Pgzero/Ironman_Game.py
@@ -4,9 +4,9 @@ from random import randint
 HEIGHT =500
 WIDTH = 600
 
-music.play("avengers")
+music.play("ironmantheme")
 p = Actor("ironman", pos=(100,100))
-c = Actor("cookie")
+c = Actor("thanos2",)
 c.x = randint(64, WIDTH-64)
 c.y = randint(64, HEIGHT-64)
 score = 0
@@ -26,24 +26,26 @@ def update():
 def update_score():
     global score
     if p.colliderect(c):
-        sounds.eating2.play()
+        sounds.shoot.play()
         score += 1
         c.pos = (randint(64,WIDTH-64),randint(64,HEIGHT-64))
+        sounds.thanoslaugh.play()
+
 
 
 
 def player_control():
     print("updating")
     if keyboard.RIGHT and not p.right > WIDTH:
-        p.angle-=10
+        p.angle=-10
         p.x += speed
     elif keyboard.LEFT and not p.left < 0:
         p.x += -speed
-        p.angle+=10
+        p.angle=10
     elif keyboard.DOWN and not p.bottom > HEIGHT:
         p.y += speed
     elif keyboard.UP and not p.top < 0:
-        p.y += -speed
+        p.y -= speed
     else:
         p.angle = 0
 
