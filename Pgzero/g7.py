b1b6e7a145907082d0af869cf65f90f17bd028d0
RayaN <rayanahmed4334@gmail.com>
2022-09-04 12:24:51 +0530

    commit

--- Pgzero/g7.py
+++ Pgzero/g7.py
@@ -12,16 +12,38 @@ class Player(Actor):
             self.x-=self.speed
         if keyboard.RIGHT and self.right <WIDTH:
             self.x+=self.speed
+        if keyboard.UP and not self.top <0:
+            self.y-=self.speed
+        if keyboard.DOWN and not self.bottom >HEIGHT:
+            self.y+=self.speed
 
+class Enemy(Actor):
+    speed = 1
+    
+    def chase(self):
+        if p.x>e.x:
+            e.x += self.speed
+        if p.x<e.x:
+            e.x -=self.speed
+        if p.y>e.y:
+            e.y += self.speed
+        if p.y<e.y:
+            e.y -=self.speed
+        if p.colliderect(e):
+            print("collision")
+        
 
 p= Player('ironman', pos=(100,100))
+e= Enemy("thanos2" , pos =(200,400))
 
 def draw():
     screen.clear()
     p.draw()
+    e.draw()
 
 def update():
     p.move()
+    e.chase()
 
 print(dir(Player))              #dir shows all functions in player Since we have actor inherited in Class player, class Player will now have all functions of Actor
 
